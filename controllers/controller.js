afdb4685eb84fa1f236d2e7d083b0768f37dfbcc
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-29 23:55:59 +0700

    ctrl : login

--- controllers/controller.js
+++ controllers/controller.js
@@ -22,5 +22,31 @@ class Controller {
             // console.log(error);
         }
     }
+
+    static async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+            // console.log(req.body);
+            if (!email || !password) throw { name: `InvalidInput` };
+
+            let user = await User.findOne({ where: { email } });
+            if (!user) throw { name: `InvalidUser` };
+            // console.log(user);
+
+            let compare = comparePass(password, user.password);
+            if (!compare) throw { name: `InvalidUser` };
+            // console.log(compare);
+
+            let token = createToken({ id: user.id });
+            // console.log(token, "token");
+
+            res.status(200).json({
+                access_token: token,
+            });
+        } catch (error) {
+            // console.log(error);
+            next(error);
+        }
+    }
 }
 module.exports = Controller;
