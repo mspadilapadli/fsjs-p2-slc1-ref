90b3f9fba9c8b60b64435af685cecda7b55b2fba
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-08 22:29:36 +0700

    helper : bcrypt async

--- helper/bcrypt.js
+++ helper/bcrypt.js
@@ -1,12 +1,21 @@
-// *sync
-const bcrypt = require("bcryptjs");
-
-let hashPassword = (password) => {
-    const salt = bcrypt.genSaltSync(8);
-    return bcrypt.hashSync(password, salt);
-};
-let comparePassword = (passwordIput, PasswordDB) => {
-    return bcrypt.compareSync(passwordIput, PasswordDB);
+//* async
+const { hash, compare } = require("bcryptjs");
+const salt = 10;
+module.exports = {
+    hashPassword: (password) => hash(password, salt),
+    comparePassword: (passwordInput, passwordDB) =>
+        compare(passwordInput, passwordDB),
 };
 
-module.exports = { hashPassword, comparePassword };
+//*sync
+// const bcrypt = require("bcryptjs");
+
+// let hashPassword = (password) => {
+//     const salt = bcrypt.genSaltSync(8);
+//     return bcrypt.hashSync(password, salt);
+// };
+// let comparePassword = (passwordIput, PasswordDB) => {
+//     return bcrypt.compareSync(passwordIput, PasswordDB);
+// };
+
+// module.exports = { hashPassword, comparePassword };
