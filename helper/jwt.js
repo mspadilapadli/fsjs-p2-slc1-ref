c7670c99ff80af3bfe35e62be66a1123d4f0a878
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-08 22:30:53 +0700

    helper: jwt without .env

---
+++ helper/jwt.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+// const secret = process.env.JWT_SECRET;
+
+const createToken = (payload) => {
+    const token = jwt.sign(payload, "secret");
+    return token;
+};
+
+const verifyToken = (token) => {
+    let payload = jwt.verify(token, "secret");
+    return payload;
+};
+
+module.exports = { createToken, verifyToken };
